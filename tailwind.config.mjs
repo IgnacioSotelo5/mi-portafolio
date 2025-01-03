3360af81f8774d5f887b3157457d06aca52e54a4
git <ignaciosotelo8@gmail.com>
2025-01-02 21:16:23 -0300

    feat: Implement sections 'Hero', 'Projects', and 'Education'. Add Navbar with navigation functions.
    - Implemented 'Hero', 'Projects', and 'Education' sections.
    - Added and configured Navbar with navigation functionalities.
    - Updated package.json and pnpm-lock.yaml.
    - Added new assets: vatar.webp and project-url-short1.webp.
    - Created new components: About.astro, Badge.astro, Content.astro, Education.astro, EducationItem.astro, Hero.astro, Navbar.astro, Pills.astro, ProjectCard.astro, Projects.astro, SectionContainer.astro, SocialLinks.astro.
    - Added new icons: Arg.astro, Brackets.astro, CapIcon.astro, Link.astro.
    - Added new logos: AstroIcon.astro, CSS.astro, Github.astro, HTML.astro, Javascript.astro, Linkedin.astro, MySQL.astro, NestJS.astro, Next.astro, Node.astro, React.astro, TailwindCSS.astro, TypeORM.astro, Typescript.astro.
    - Removed Welcome.astro.
    - Updated Layout.astro and index.astro.
    - Added new stylesheet: index.css.
    - Updated 	ailwind.config.mjs and 	sconfig.json.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				foreground: {
+					100: "#fffdfb",
+    				150: "#fff9f4",
+    				200: "#ffedde",
+    				250: "#f2ddcc",
+    				300: "#e3cbbc",
+    				350: "#bba5a0",
+    				400: "#a2898c",
+    				450: "#867073",
+    				550: "#6d585b",
+    				600: "#604d4f",
+    				650: "#524144",
+    				700: "#463739",
+    				750: "#392d2e",
+    				800: "#2d2224",
+    				850: "#21191a",
+    				900: "#150f10",
+			}
+		},
+		keyframes: {
+			navShadown: {
+					'0%': { padding: '0 0.5rem' },
+					'100%': { 
+						backgroundColor: 'rgba(71, 85, 105, 0.5)', 
+						padding: '0 12px', 
+						backdropFilter: 'blur(4px)', 
+						zIndex: '30',
+						borderRadius: '9999px'
+					}
+			},
+		},
+		animation: {
+			navShadown: 'navShadown 1s linear both'
+		}
+		},
 	},
 	plugins: [],
 }
